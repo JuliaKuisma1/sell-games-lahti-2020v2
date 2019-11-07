94f7eb93e7bbe25a85b2d4dbcd822b3854bf55a7
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-11-07 16:01:37 +0200

    Single page initialised

--- single.php
+++ single.php
@@ -1,1 +1,27 @@
-moi
+<?php get_header(); ?>
+  <main id="single-content">
+    <?php
+      if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+        ?> 
+          <div class="single-card card">
+            <?php 
+              if ( has_post_thumbnail() ) {
+                the_post_thumbnail('large', array('class' => 'card-img-top single-image')); 
+              } ?>
+            <div class="card-body">
+              <h1 class="card-title">
+                <?php the_title(); ?>
+              </h1>
+              <?php the_content(); ?>
+            </div>
+          </div>
+          <?php
+          endwhile; 
+          else :
+            echo "Nothing found";
+          endif;
+    
+         ?>
+  </main>
+<?php get_footer(); ?>

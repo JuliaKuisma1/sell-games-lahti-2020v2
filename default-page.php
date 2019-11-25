ae8cf97d4d003f80b49c0552bed9540c0a15ffb4
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2019-11-25 18:53:36 +0200

    Modified subpage menu

--- default-page.php
+++ default-page.php
@@ -1,15 +1,20 @@
 <?php
-/* Template Name: Default page */
+/* Template Name: Parent page */
 
 get_header(); ?>
-<div class="main-content">
-    <div>
+<div class="subpage-container">
+    <div class="subpage-content">
         <?php echo do_shortcode('[wpb_childpages]'); ?>
     </div>
+</div>
+    
+<div class="main-content">
     <?php if ( have_posts() ) {
             while ( have_posts() ) {
                 the_post(); ?> 
-                    <?php the_post_thumbnail();?>
+                    <div class="post_image">
+                        <?php the_post_thumbnail();?>
+                    </div>
                     <h2><?php the_title(); ?></h2>
                     <?php the_content(); ?>
                 

aebad24d7e858c63aff07ace81a3bcf8220b9978
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2019-11-25 19:20:12 +0200

    Modified subpage menu and parent link

--- default-page.php
+++ default-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
     <?php if ( have_posts() ) {
             while ( have_posts() ) {
                 the_post(); ?> 
-                    <div class="post_image">
+                    <div class="post-image">
                         <?php the_post_thumbnail();?>
                     </div>
                     <h2><?php the_title(); ?></h2>
--- subpage-page.php
+++ subpage-page.php
@@ -2,6 +2,17 @@
 /* Template Name: Subpage */
 
 get_header(); ?>
+<div class="subpage-container">
+    <div class="subpage-content">
+        <?php if ( $post->post_parent ) { ?>
+        <div class="parent-link">
+            <a href="<?php echo get_permalink( $post->post_parent ); ?>" >
+                <p> <-- </p><?php echo get_the_title( $post->post_parent ); ?>
+            </a>
+        </div>
+        <?php } ?>
+    </div>
+</div>
 
 <div class="main-content">
 <?php // implement path?

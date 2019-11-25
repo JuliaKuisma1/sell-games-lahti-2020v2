110088927a0dfbafda3018dbb396747c8d26c264
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2019-11-25 08:26:58 +0200

    Modified default page and added childlist function

--- default-page.php
+++ default-page.php
@@ -1,17 +1,14 @@
 <?php
-/* Template name: Default page */
+/* Template Name: Default page */
 ?>
 
 <div class="main-container">
 <?php 
+    [wpb_childpages]
       if ( have_posts() ) :
-        while ( have_posts() ) : the_post(); 
-          if (!is_front_page() && !is_home()) {
-            ?>
-              <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-            <?php
-          }
-          the_content(); 
+        while ( have_posts() ) : the_post(); ?>
+            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <?php the_content(); 
         endwhile; 
       else :
         echo "Nothing found";

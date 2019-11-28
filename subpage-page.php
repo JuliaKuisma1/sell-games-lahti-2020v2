5d67bb713cd0a4b7927d4e82149d5a404e812da7
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-11-28 09:20:25 +0200

    Submenu changed to bootstrap menu

--- subpage-page.php
+++ subpage-page.php
@@ -2,33 +2,17 @@
 /* Template Name: Subpage */
 
 get_header(); ?>
-<!--
-<div class="subpage-container">
-    <div class="subpage-content">
-        <?php if ( $post->post_parent ) { ?>
-        <div class="parent-link">
-            <a href="<?php echo get_permalink( $post->post_parent ); ?>" >
-                <p> <-- </p><?php echo get_the_title( $post->post_parent ); ?>
-            </a>
-        </div>
-        <?php } ?>
-    </div>
-</div>
--->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav id="sub-navigation" class="navbar navbar-expand-lg navbar-light bg-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#subnav" aria-controls="subnav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="subnav">
     <div class="navbar-nav">
-      <?php if ( $post->post_parent ) { ?>
-            <a class="nav-item nav-link" href="<?php echo get_permalink( $post->post_parent ); ?>" >
-              <?php echo get_the_title( $post->post_parent ); ?>
-            </a>
-      <?php } ?>
+      <?php echo do_shortcode('[wpb_childpages]'); ?>
     </div>
   </div>
 </nav>
+
 <div class="main-content">
 <?php // implement path?
     if ( have_posts() ) {

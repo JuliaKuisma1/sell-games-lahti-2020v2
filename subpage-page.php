720de0690a6758b6ee22287c03dd58f0fd6e4deb
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-11-28 09:38:07 +0200

    Changed subnav toggler icon

--- subpage-page.php
+++ subpage-page.php
@@ -4,7 +4,7 @@
 get_header(); ?>
 <nav id="sub-navigation" class="navbar navbar-expand-lg navbar-light bg-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#subnav" aria-controls="subnav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <i class="fas fa-bars"></i>
   </button>
   <div class="collapse navbar-collapse" id="subnav">
     <div class="navbar-nav">

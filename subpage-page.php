f897e3e937ed4677981b502d6ccf9b907d386020
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-11-28 09:00:03 +0200

    Replacing the subnav with bootstrap's nav

--- subpage-page.php
+++ subpage-page.php
@@ -2,6 +2,7 @@
 /* Template Name: Subpage */
 
 get_header(); ?>
+<!--
 <div class="subpage-container">
     <div class="subpage-content">
         <?php if ( $post->post_parent ) { ?>
@@ -13,6 +14,21 @@ get_header(); ?>
         <?php } ?>
     </div>
 </div>
+-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <?php if ( $post->post_parent ) { ?>
+            <a class="nav-item nav-link" href="<?php echo get_permalink( $post->post_parent ); ?>" >
+              <?php echo get_the_title( $post->post_parent ); ?>
+            </a>
+      <?php } ?>
+    </div>
+  </div>
+</nav>
 
 <div class="main-content">
 <?php // implement path?

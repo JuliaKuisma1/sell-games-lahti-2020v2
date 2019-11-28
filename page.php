a4edb426caf73311f0285141c766387009f5047a
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-11-28 08:21:40 +0200

    Remove excessive header from the template

--- page.php
+++ page.php
@@ -8,7 +8,6 @@ get_header(); ?>
     while ( have_posts() ) {
         the_post();
         the_post_thumbnail();?>
-        <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
         </div>
     <?php }

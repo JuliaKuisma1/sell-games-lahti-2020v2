b1db0c9a86352ceb2349d48299324257edde2e29
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-12-05 10:08:00 +0200

    Added bootstrap grid

--- page.php
+++ page.php
@@ -4,14 +4,21 @@
 get_header(); ?>
 
 <div class="main-content">
-<?php if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post();  
-        the_post_thumbnail();?>
-        <?php the_content(); ?>
-        </div>
-    <?php }
-} ?>  
+  <div class="row">
+    <div id="main-content-container" class="col-lg-8 col-sm-12">
+      <?php if ( have_posts() ) {
+          while ( have_posts() ) {
+              the_post();  
+              the_post_thumbnail();?>
+              <?php the_content(); ?>
+              </div>
+          <?php }
+      } ?>  
+      </div>
+      <div id="sidebar-container" class="col-lg-4 col-sm-12">
+        <?php dynamic_sidebar( 'sidebar' ); ?>
+      </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>

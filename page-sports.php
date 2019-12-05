9eefc8b39c8190262d38ec609aee92fb2e17b674
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-12-05 08:35:25 +0200

    Removed extra tag closure

--- page-sports.php
+++ page-sports.php
@@ -16,16 +16,15 @@ get_header(); ?>
 <div class="main-content">
   <div class="row">
     <div id="main-content-container" class="col-lg-8 col-sm-12">
-      <?php // implement path?
+      <?php
       if ( have_posts() ) {
-          while ( have_posts() ) {
-              the_post(); ?> 
-                  <?php the_post_thumbnail();?>
-                  <h2><?php the_title(); ?></h2>
-                  <?php the_content(); ?>
-              </div>
-          <?php
-          }
+        while ( have_posts() ) {
+          the_post(); ?> 
+          <?php the_post_thumbnail();?>
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+        <?php
+        }
       } ?>
     </div>
     <div id="sidebar-container" class="col-lg-4 col-sm-12">

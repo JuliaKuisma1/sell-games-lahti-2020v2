4ed2a911106140b7aa5f4920b129703153b1fb9c
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-12-05 09:44:26 +0200

    Card deck class added

--- page-sports.php
+++ page-sports.php
@@ -24,36 +24,36 @@ get_header(); ?>
           <h2><?php the_title(); ?></h2>
           <?php the_content(); ?>
 
-
-          <?php
-          $id = get_the_ID();
-          $args = array(
-              'post_type'      => 'page',
-              'posts_per_page' => -1,
-              'post_parent'    => $id,
-              'order'          => 'ASC',
-              'orderby'        => 'menu_order'
-          );
-          $parent = new WP_Query( $args );
-          if ( $parent->have_posts() ) : ?>
-              <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
-                  <?php if (get_the_ID() == 47 || get_the_ID() == 61) continue; ?>
-                      <div class="sport-card card">
-                        <a href="<?php the_permalink(); ?>">
-                          <?php 
-                            if ( has_post_thumbnail() ) {
-                              the_post_thumbnail('medium', array('class' => 'card-img-top')); 
-                            } else {
-                              ?>
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/img-placeholder-medium.png">
-                              <?php
-                            }
-                          ?>
-                        </a>
-                      </div>
-              <?php endwhile; ?>
-          <?php endif; ?>
-
+          <div class="card-deck">
+            <?php
+            $id = get_the_ID();
+            $args = array(
+                'post_type'      => 'page',
+                'posts_per_page' => -1,
+                'post_parent'    => $id,
+                'order'          => 'ASC',
+                'orderby'        => 'menu_order'
+            );
+            $parent = new WP_Query( $args );
+            if ( $parent->have_posts() ) : ?>
+                <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
+                    <?php if (get_the_ID() == 47 || get_the_ID() == 61) continue; ?>
+                        <div class="sport-card card">
+                          <a href="<?php the_permalink(); ?>">
+                            <?php 
+                              if ( has_post_thumbnail() ) {
+                                the_post_thumbnail('medium', array('class' => 'card-img-top')); 
+                              } else {
+                                ?>
+                                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/img-placeholder-medium.png">
+                                <?php
+                              }
+                            ?>
+                          </a>
+                        </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+          </div>
         <?php
         }
       } ?>

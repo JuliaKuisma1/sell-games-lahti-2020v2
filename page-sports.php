b1f636864bfeb3748fa98174675b98bf2b4b74e0
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-12-05 08:05:47 +0200

    Added subnav

--- page-sports.php
+++ page-sports.php
@@ -2,6 +2,16 @@
 /* Template Name: Sports */
 
 get_header(); ?>
+<nav id="sub-navigation" class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#subnav" aria-controls="subnav" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fas fa-bars"></i>
+  </button>
+  <div class="collapse navbar-collapse" id="subnav">
+    <div class="navbar-nav">
+      <?php echo do_shortcode('[wpb_childpages]'); ?>
+    </div>
+  </div>
+</nav>
 
 <div class="main-content">
 <?php // implement path?
@@ -16,6 +26,5 @@ get_header(); ?>
         }
     } ?>  
 </div>
-sports
 
 <?php get_footer(); ?>

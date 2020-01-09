177205244aae3527723e25c640634023dbb51935
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2020-01-09 15:26:34 +0200

    changes in images

--- default-page.php
+++ default-page.php
@@ -18,7 +18,6 @@ get_header(); ?>
       <?php if ( have_posts() ) {
             while ( have_posts() ) {
                 the_post(); ?> 
-                  <?php the_post_thumbnail(); ?>
                   <h2><?php the_title(); ?></h2>
                   <?php the_content(); ?>
             <?php

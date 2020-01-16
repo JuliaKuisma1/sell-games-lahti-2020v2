6f332a0dc81a8ea64d0d9918a0427770ac9f824b
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2020-01-16 11:23:24 +0200

    Modified sports page

--- page-sports.php
+++ page-sports.php
@@ -2,16 +2,6 @@
 /* Template Name: Sports */
 
 get_header(); ?>
-<nav id="sub-navigation" class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#subnav" aria-controls="subnav" aria-expanded="false" aria-label="Toggle navigation">
-    <i class="fas fa-bars"></i>
-  </button>
-  <div class="collapse navbar-collapse" id="subnav">
-    <div class="navbar-nav">
-      <?php echo do_shortcode('[wpb_sports_childpages]'); ?>
-    </div>
-  </div>
-</nav>
 
 <div class="main-content">
   <div class="row">
@@ -62,9 +52,6 @@ get_header(); ?>
         <?php
         }
       } ?>
-    </div>
-    <div id="sidebar-container" class="col-lg-4 col-sm-12">
-      <?php dynamic_sidebar( 'sidebar' ); ?>
     </div>
   </div>
 </div>

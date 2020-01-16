ec92c668166ae81b092c57997403b5fa8cd6971f
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2020-01-16 11:20:17 +0200

    Removed nav in subpage

--- subpage-page.php
+++ subpage-page.php
@@ -2,16 +2,6 @@
 /* Template Name: Subpage */
 
 get_header(); ?>
-<nav id="sub-navigation" class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#subnav" aria-controls="subnav" aria-expanded="false" aria-label="Toggle navigation">
-    <i class="fas fa-bars"></i>
-  </button>
-  <div class="collapse navbar-collapse" id="subnav">
-    <div class="navbar-nav">
-      <?php echo do_shortcode('[wpb_childpages]'); ?>
-    </div>
-  </div>
-</nav>
 
 <div class="main-content">
 

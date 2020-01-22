a3a9384a6eb927cc8e9d20c9c3b9f01ece10b87c
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2020-01-22 12:07:07 +0200

    Modified footer

--- footer.php
+++ footer.php
@@ -1,5 +1,40 @@
-    <footer id="main-footer">
-      <p>© SELL Games 2020</p>
+    <footer id="main-footer" style="background-color: yellow">
+    <div class="footer-container">
+      <div class="row">
+        <div class="col-sm-6 col-md-5 col-xs-12">
+          <a href="<?php echo home_url(); ?>">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/SELLGAMES_logo.png" height="90px" alt="Sell Games Lahti 2020">
+          </a>
+        </div>
+        <div class="col-sm-6 col-md-5 col-xs-12">
+          <ul>
+            <?php
+                  /**
+                   * Displays the main menu
+                   * https://developer.wordpress.org/reference/functions/wp_nav_menu/
+                   */
+                  $args = array(
+                    'menu' => 'main-menu',
+                    'menu_id' => 'main-menu',
+                    'container' => 'nav',
+                    'depth' => 1,
+                    'echo' => false,
+                    'wrap-class' => 'footer-navigation',
+                    'items_wrap' => '%3$s'
+                  );
+                  echo strip_tags( wp_nav_menu( $args ), '<a><li>' );
+              ?>
+          </ul>
+        </div>
+        <div class="col-6 col-md-2 col-xs-12">
+          <div class="sosial-container">
+            <a href="https://www.facebook.com/SELLgames2020/"><i class="fab fa-facebook-square fa-2x"></i></a>
+            <a href="https://www.instagram.com/sellgames2020/"><i class="fab fa-instagram fa-2x"></i><a>
+          </div>
+        </div>
+      </div>
+    </div>
+      
     </footer>
     <!-- Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- header.php
+++ header.php
@@ -19,11 +19,11 @@
         } 
       ?>
     </title>
-    <!-- Favorite icon -->
-    <link rel="shortcut icon" type="image/ico" href="./images/favicon.ico">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <!-- Fontawesome -->
+    <script src="https://use.fontawesome.com/833ae2799f.js"></script>
     <!-- WordPress Header -->
     <?php wp_head(); ?>
   </head>

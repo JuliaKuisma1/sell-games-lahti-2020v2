178197f893f2eeb9bce132d1ed791e3cb42f03ba
JuliaKuisma1 <julia.kuisma@student.lamk.fi>
2020-01-29 12:05:15 +0200

    Added EUSA logo

--- functions.php
+++ functions.php
@@ -78,6 +78,15 @@
       'before_title'  => '<h2>',
       'after_title'   => '</h2>',
     ) );
+
+    register_sidebar( array(
+      'name'          => 'Partners',
+      'id'            => 'sidebar_partners',
+      'before_widget' => '<div class="partner-widget">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h2>',
+      'after_title'   => '</h2>',
+    ));
   
   }
   add_action( 'widgets_init', 'arphabet_widgets_init' );
--- page-single-sport.php
+++ page-single-sport.php
@@ -5,7 +5,7 @@ get_header(); ?>
 
 <div class="main-content">
   <div class="row">
-    <div id="main-content-container" class="col-lg-8 col-sm-12">
+    <div id="main-content-container" class="col-lg-8 col-md-7 col-sm-12">
       <?php
       if ( have_posts() ) {
         while ( have_posts() ) {
@@ -17,8 +17,8 @@ get_header(); ?>
         }
       } ?>
     </div>
-    <div id="sidebar-container" class="col-lg-4 col-sm-12">
-      <?php dynamic_sidebar( 'sidebar' ); ?>
+    <div id="sidebar-container" class="col-lg-4 col-md-5 col-sm-12">
+      <?php dynamic_sidebar( 'sidebar_tabletennis' ); ?>
     </div>
   </div>
 </div>

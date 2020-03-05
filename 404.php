0f2bfd43667d54ecded5ba8a71010590c849604f
EetuAndronoff <eetu.andronoff@student.lab.fi>
2020-03-05 10:12:28 +0200

    Added 404 page and cleaned up searchpage

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<head>
+    <title>Page not found</title>
+</head>
+
+<body>
+    <div class="main-content">
+        <div id="main-content-container">
+            <div class="row">
+            <img src="http://localhost/wordpress/wp-content/uploads/2020/03/zest_winter.png" alt="zest" height="300" width="300">
+            <div class="col">
+            <h2>Sorry!</h2><h3>We couldn't find what you were looking for.</h3>
+            <br>
+            <a href="index.php"><button class="category">Return to the front page</button></a>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<?php get_footer(); ?>
--- searchpage.php
+++ searchpage.php
@@ -4,8 +4,6 @@
 get_header(); ?>
 
 <div class="main-content">
-<a href="../category/news/"><button>News</button></a>
-<a href="../category/interview/"><button>Interviews</button></a>
   <?php get_search_form(); ?>
 </div>
 
